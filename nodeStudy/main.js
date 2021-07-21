c6c0a1a44c03271422e30470cf74a21b35a0e06d
Jihyeon <gihyeon0715@naver.com>
2021-07-21 11:08:03 +0900

    Node.js

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -11,6 +11,7 @@ console.log(m.divide(1,2));
 */
 
 /*
+//코어 모듈
 const fs=require('fs'); //filesystem
 let fileList=fs.readdirSync('.');
 console.log(fileList);
@@ -18,9 +19,26 @@ fs.writeFileSync('new','Hello Node.js!');
 
 const os=require('os'); //운영체제
 console.log(os.cpus());
-*/
 
+
+//서드파티 모듈
 const cowsay=require('cowsay');
 console.log(cowsay.say({
   text:"I love javascript",
 }));
+*/
+
+
+const fs=require('fs');
+
+console.log('Start');
+
+/*
+let content=fs.readFileSync('./new','utf8'); //동기실행함수
+console.log(content);
+*/
+
+fs.readFile('./new','utf8',(error,data)=>{ //비동기실행함수-> node.js는 보통 비동기 실행 함수 사용
+  console.log(data);
+})
+console.log('Finish');
