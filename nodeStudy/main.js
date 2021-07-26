3ac3827b28e9b9a90865f436b733c5c3d28bc021
Jihyeon <gihyeon0715@naver.com>
2021-07-26 14:22:59 +0900

    Node.js

--- nodeStudy/main.js
+++ nodeStudy/main.js
@@ -93,10 +93,12 @@ myEmitter.emit('test1',obj);
 
 
 //웹 서버 만들기
+/*
 const http=require('http');
 
 let server=http.createServer(function(request,response){
-  response.end('<h1>Hello wolrd!</h1>');
+  console.log(request.url);
+  //response.end('<h1>Hello wolrd!</h1>');
 });
 server.listen(3000);//외부로부터 오는 클라이언트의 요청을 받아들일 준비(포트번호 3000)
 
@@ -105,4 +107,60 @@ console.log(url.protocol);
 console.log(url.host);
 console.log(url.pathname);
 console.log(url.search);
+*/
 
+
+//라우팅 해보기
+/*
+const http = require('http'); // etc
+
+const users = ['Tom', 'Andy', 'Jessica', 'Paul']; // const
+
+const server = http.createServer((request, response) => { // Arrow Function, const
+  if (request.url === '/') {
+    response.end('<h1>Welcome!</h1>');
+  } else if (request.url === '/users') {
+    response.end(`<h1>${users}</h1>`); // Template String
+  } else if (request.url.split('/')[1] === 'users') {
+    // url : /users/1, /users/2, .. // etc
+    const userIdx = request.url.split('/')[2];
+    const userName = users[userIdx - 1]; // etc
+
+    response.end(`<h1>${userName}</h1>`); // Template String
+  } else {
+    response.end('<h1>Page Not Available</h1>');
+  }
+});
+
+server.listen(3000);
+*/
+
+
+//express 모듈로 더 세련된 라우팅
+const http = require('http'); // etc
+
+const express=require('express');
+const app=express();
+
+const users = ['Tom', 'Andy', 'Jessica', 'Paul'];
+
+app.get('/',(request,response)=>{
+  response.end(`<h1>Welcome!</h1>`);
+});
+
+app.get('/users',(request,response)=>{
+  response.end(`<h1>${users}</h1>`);
+});
+
+app.get('/users/:id',(request,response)=>{
+
+  //console.log(request.params);
+  const userName=users[request.params.id-1];
+  response.end(`<h1>${userName}</h1>`);
+});
+
+app.get('*',(request,response)=>{
+  response.end('<h1>Page Not Available</h1>');
+});
+
+app.listen(3000);
